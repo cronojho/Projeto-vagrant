37b5c4fe3377a929789ad0e8a6aa1d37f5b57f92
cronojho <cronojho@gmail.com>
2023-02-21 13:13:29 -0300

    Vagrant 1.0

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+    docker swarm join --token SWMTKN-1-4q8ttcoijkk0t3tq51kvtwc4nmxvdzhrci03j060clxo0q7cjw-dlv8qarex2ho8r9s2l25mmsk1 10.10.10.100:2377
